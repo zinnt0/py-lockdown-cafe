cc1d1e794df7dce911cdf701744b75b85cff2f1a
Tristan Zinn <tristanzinn@MacBook-Pro-von-Tristan.local>
2025-04-13 02:19:49 +0200

    Another Solution

--- app/main.py
+++ app/main.py
@@ -8,15 +8,15 @@ def go_to_cafe(friends: list, cafe: Cafe) -> None:
     masks_to_buy = 0
 
     for friend in friends:
-        if not friend.get("wearing_a_mask", False):
-            masks_to_buy += 1
+        # if not friend.get("wearing_a_mask", False):
+        #     masks_to_buy += 1
 
         try:
             cafe.visit_cafe(friend)
         except (NotVaccinatedError, OutdatedVaccineError):
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            pass
+            masks_to_buy += 1
 
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
